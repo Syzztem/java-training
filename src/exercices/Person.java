3b1359e852fd2598673ca68bd6af7b101726b713
Sysstem <louis.thievent@gmail.com>
2018-11-12 13:00:30 +0100

    solution

--- src/exercices/Person.java
+++ src/exercices/Person.java
@@ -1,42 +1,52 @@
 package exercices;
 
-public class Person {
+public class Person 
+{
+	private String name;
+	private int age;
+	private boolean wilder;
 
-    // TODO 1. Add the following private attributes :
-    // - name (String)
-    // - age (int)
-    // - wilder (boolean)
+	public Person(String name, int age)
+	{
+		this.name = name;
+		this.age = age;
+	}
 
+	public String getName()
+	{
+		return this.name;
+	}
 
-    // TODO 2. Add a Constructor with name and age arguments (in this order)
-    // This constructor must initialize name and age attributes
+	public int getAge()
+	{
+		return this.age;
+	}
 
+	public boolean isWilder()
+	{
+		return this.wilder;
+	}
 
-    // TODO 3. Add getter method for name attribute
+	public void setName(String name)
+	{
+		this.name = name;
+	}
 
+	public void setAge(int age)
+	{
+		this.age = age;
+	}
 
-    // TODO 4. Add setter method for name attribute
+	public void setWilder(boolean wilder)
+	{
+		this.wilder = wilder;
+	}
 
+	public String whoAmI()
+	{
+		return ("My name is " + name + " and I'm " + age);
+	}
 
-    // TODO 5. Add getter method for age attribute
-
-
-    // TODO 6. Add setter method for age attribute
-
-
-    // TODO 7. Add getter method for wilder attribute
-
-
-    // TODO 8. Add setter method for wilder attribute
-
-
-    // TODO 9. Create a method 'whoAmI' that returns "My name is {name} and I'm {age}"
-    // where you replace {name} and {age} with attributes values
-
-
-
-
-    // Do not remove this empty constructor !
     public Person() {
     }
 }
